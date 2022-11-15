0a051f06c982488d431340c38fadb78a4e52f95c
poorna <poorna029@gmail.com>
2022-11-15 13:09:20 +0000

    variable poorna is added

--- importFunction.js
+++ importFunction.js
@@ -1,2 +1,5 @@
 const exp = require("./exportFunction");
 // console.log(exp);
+// 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
+// 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
+const poorna = "I love coding";
